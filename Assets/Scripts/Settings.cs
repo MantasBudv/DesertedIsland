b4c51ecb1692490d7442a97a80a362eee01a5d8e
GintasStasauskas <gintas.stasauskas@ktu.edu>
2021-02-25 21:24:35 +0200

    Added main menu and music

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+public class Settings : MonoBehaviour
+{
+    public GameObject box;
+    public AudioMixer AudioMixer;
+
+    public void ButtonToggle()
+    {
+        if (box.activeInHierarchy)
+            box.SetActive(false);
+        else box.SetActive(true);
+    }
+
+    public void SetMusic(float volume)
+    {
+        AudioMixer.SetFloat("Music", volume);
+    }
+    public void SetSounds(float volume)
+    {
+        AudioMixer.SetFloat("Sounds", volume);
+    }
+
+}
