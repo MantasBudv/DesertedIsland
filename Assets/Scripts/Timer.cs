706c6653850eede9045b199af0ed28ad7c2e1cf0
Neda Montvydaitė <neda.montvydaite@ktu.edu>
2021-04-12 15:10:52 +0300

    Menu bar

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
     public UnityEngine.UI.Image img;
-    public float timeStart;
-    public int day;
+    public static float timeStart = 1200;
+    public static int day = 1;
     public Text textBox;
     public Text textBox2;
     public Color Morning;
@@ -17,10 +18,13 @@ public class Timer : MonoBehaviour
     public Color Evening1;
     public Color Evening2;
     public Color Night;
+    public static Timer instance;
+
     // Start is called before the first frame update
     void Start()
     {
         textBox.text = TimeSpan.FromMinutes(timeStart).ToString(@"hh\:mm");
+        changeSky();
         textBox2.text = "Day " + day.ToString();
     }
 
@@ -30,7 +34,7 @@ public class Timer : MonoBehaviour
         
         timeStart += Time.deltaTime*5;
         Debug.Log(Mathf.Round(timeStart/10)*10);
-        textBox.text =TimeSpan.FromMinutes(Mathf.Round(timeStart/10)*10).ToString(@"hh\:mm");
+        textBox.text = TimeSpan.FromMinutes(Mathf.Round(timeStart/10)*10).ToString(@"hh\:mm");
         changeSky();
         if(Mathf.Round(timeStart/10)*10 == 1440){
             timeStart = 0;
@@ -42,30 +46,48 @@ public class Timer : MonoBehaviour
 
     void changeSky()
     {
-        switch (Mathf.Round(timeStart))
+        var t = Mathf.Round(timeStart);
+
+        //06:00 - 08:59
+        if (t >= 360 && t < 540)
         {
-          case 360: //06:00
-              img.color = Morning;
-              break;
-          case 540: //09:00
-              img.color = Morning2;
-              break;
-          case 720: //12:00
-              img.color = Noon;
-              break;
-          case 1080:// 18:00
-              img.color = Evening1;
-              break;     
-          case 1200:// 20:00
-              img.color = Evening2;
-              break;     
-          case 1320:// 22:00
-              img.color = Night;
-              break;     
-          default:
-              break;
+            img.color = Morning;
         }
-
-
+        else
+        {
+            //09:00 - 11:59
+            if (t >= 540 && t < 720)
+            {
+                img.color = Morning2;
+            }
+            else
+            {
+                //12:00 - 17:59
+                if (t >= 720 && t < 1080)
+                {
+                    img.color = Noon;
+                }
+                else
+                {
+                    //18:00 - 19:59
+                    if (t >= 1080 && t < 1200)
+                    {
+                        img.color = Evening1;
+                    }
+                    else
+                    {
+                        //20:00 - 21:59
+                        if (t >= 1200 && t < 1320)
+                        {
+                            img.color = Evening2;
+                        }
+                        else
+                        {
+                            img.color = Night;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
