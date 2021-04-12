c3cdfdaa0006ea69025f3e54344eaccb54a3ef20
Vilius <vilius.sabonis@ktu.edu>
2021-04-12 21:55:50 +0300

    praeito commit fix

--- Assets/Scripts/SkillTreeUISkillButton.cs
+++ Assets/Scripts/SkillTreeUISkillButton.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class SkillTreeUISkillButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void TestClick()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("!!!");
     }
 }
