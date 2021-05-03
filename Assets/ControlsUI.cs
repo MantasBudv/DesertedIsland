ad912d0c77ce6ad4e6891aef1caecbaf06607591
Mantas Būdvytas <m.budvytas@gmail.com>
2021-05-03 20:25:52 +0300

    created GUI screen for controls

---
+++ Assets/ControlsUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlsUI : MonoBehaviour
+{
+    public GameObject controlsUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            controlsUI.SetActive(!controlsUI.activeSelf);
+        }
+    }
+}
