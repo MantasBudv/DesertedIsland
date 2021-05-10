a91651020c8c82dde171cbbb39c17308f1d0d070
Tomasl33 <tomas.linikas@gmail.com>
2021-05-10 13:58:50 +0300

    Added minigame, fixed hotbar items

---
+++ Assets/AdjustFishingLine.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustFishingLine : MonoBehaviour
+{
+    [SerializeField] Transform hook;
+    float distance = 5.52f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.localScale = new Vector3(1, (this.transform.position.y - hook.position.y) / distance, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.localScale = new Vector3(1, (this.transform.position.y - hook.position.y) / distance, 1);
+    }
+}
