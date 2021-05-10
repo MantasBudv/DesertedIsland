07fe292740ae818c26f1788e4e82fe620879b2c0
GintasStasauskas <gintas.stasauskas@ktu.edu>
2021-05-10 03:25:39 +0300

    added boat

---
+++ Assets/Scripts/GMStaticValues.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GMStaticValues : MonoBehaviour
+{
+    private static int boatLevel = 1;
+
+    public void SetBoatLevel(int level)
+    {
+        boatLevel = level;
+    }
+
+    public int GetBoatLevel()
+    {
+        return boatLevel;
+    }
+}
